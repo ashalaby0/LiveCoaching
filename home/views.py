4d1c51ba2cb028f16a749fc0cf41a9906949a343
ahmed <ahmed.shalaby241@gmail.com>
2022-11-15 23:53:45 +0200

    pep8 formating

--- home/views.py
+++ home/views.py
@@ -1,21 +1,26 @@
-from django.shortcuts import render
-from django.db.models import Q
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.shortcuts import render
 
-from . import models
+from home import models
+
 
 @login_required(login_url='/accounts/login')
 def index(request):
 
-    coach_name_q = request.GET.get('coach_name_q') if request.GET.get('coach_name_q') != None else ''
-    coach_speciality_q = request.GET.get('coach_speciality_q') if request.GET.get('coach_speciality_q') != None else ''
-    min_price_q = request.GET.get('min_price_q') if request.GET.get('min_price_q') != None else 0
-    max_price_q = request.GET.get('max_price_q') if request.GET.get('max_price_q') != None else 1000
+    coach_name_q = request.GET.get('coach_name_q') if request.GET.get(
+        'coach_name_q') != None else ''
+    coach_speciality_q = request.GET.get('coach_speciality_q') if request.GET.get(
+        'coach_speciality_q') != None else ''
+    min_price_q = request.GET.get('min_price_q') if request.GET.get(
+        'min_price_q') != None else 0
+    max_price_q = request.GET.get('max_price_q') if request.GET.get(
+        'max_price_q') != None else 1000
     coach_list = models.Coach.objects.filter(
-        Q(user__username__icontains=coach_name_q) & 
+        Q(user__username__icontains=coach_name_q) &
         Q(speciality__icontains=coach_speciality_q) &
         Q(price_per_hour__gte=min_price_q) &
-        Q(price_per_hour__lte=max_price_q) 
+        Q(price_per_hour__lte=max_price_q)
     )
     return render(
         request=request,

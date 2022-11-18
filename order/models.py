cd8e6d236318305edd983042565e628f15d8f83d
ahmed <ahmed.shalaby241@gmail.com>
2022-11-19 00:41:40 +0200

    adjusting sessions page

--- order/models.py
+++ order/models.py
@@ -4,58 +4,60 @@ from creditcards.models import (CardExpiryField, CardNumberField,
 from django.contrib.auth.models import User
 from django.db import models
 
-#TODO
+# TODO
 # login .. done
 # register .. done
 # password reset .. done
-# payment .. test done { pending on read payment test} 
+# payment .. test done { pending on read payment test}
 # pages:
-    # home page .. in progress [ search, add contactus link ]
-    # contact us page
-    # coach registeration page ... pending
-    # coach profile page .. in progress
-    # client profile page .. pending
-    # search .. done
-    # filter .. in progress [availability]
-    # scheduled sessions page .. pending
-    # booking .. in progress
-    
+# home page .. in progress [ search, add contactus link ]
+# contact us page
+# coach registeration page ... pending
+# coach profile page .. in progress
+# client profile page .. pending
+# search .. done
+# filter .. in progress [availability]
+# scheduled sessions page .. pending
+# booking .. in progress
+
 # session
-    # start_time
-    # status
-    # duration
-    # coach
-    # client_set
-    
+# start_time
+# status
+# duration
+# coach
+# client_set
+
 
 # client ( extend user mode )
-    # name
-    # email
-    # gender
-    # ..
-    
+# name
+# email
+# gender
+# ..
+
 # coach ( extend user mode )
-    # name
-    # speciality
-    # rating
-    # location
-    # price_per_hour
-    # price_per_30
-    # available_for_kids
+# name
+# speciality
+# rating
+# location
+# price_per_hour
+# price_per_30
+# available_for_kids
 
 
 class Order(models.Model):
-    name  = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_finished = models.BooleanField(default=False)
     price = models.IntegerField(default=0)
 
+
 class OrderDetails(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
 
+
 class Payment(models.Model):
     models.ForeignKey(Order, on_delete=models.CASCADE)
-    shipment_address  = models.CharField(max_length=150)
+    shipment_address = models.CharField(max_length=150)
     chipment_phone = models.CharField(max_length=150)
     card_number = CardNumberField()
     expiry_date = CardExpiryField()
@@ -64,13 +66,13 @@ class Payment(models.Model):
     def _paymob_first_api_call(self):
         url = 'https://accept.paymob.com/api/auth/tokens'
         context = {"api_key": "ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnVZVzFsSWpvaWFXNXBkR2xoYkNJc0luQnliMlpwYkdWZmNHc2lPall5TURFd05Dd2lZMnhoYzNNaU9pSk5aWEpqYUdGdWRDSjkuNFpXTDFTemZfWC1FUEowcUtldXZ1VVN0WlJrU1dNNm0zRXFGZFlzNlZvS3ZZaEFBcFpSMGg1cURvVkNIZkd2MWFJUWFBSWRJbjZZaFlmejJwMkdqdEE="}
-        r = requests.post(url, json = context)
+        r = requests.post(url, json=context)
         token = r.json().get('token')
         print(token)
         if token:
             return True, token
         return False, token
-        
+
     def _paymob_seccond_api_call(self, token, merchant_order_id):
         print(merchant_order_id)
         url = 'https://accept.paymob.com/api/ecommerce/orders'
@@ -87,41 +89,41 @@ class Payment(models.Model):
                     "description": "Smart Watch",
                     "quantity": "1"
                 },
-                { 
+                {
                     "name": "ERT6565",
                     "amount_cents": "200000",
                     "description": "Power Bank",
                     "quantity": "1"
                 }
-                ],
+            ],
             "shipping_data": {
-                "apartment": "803", 
-                "email": "claudette09@exa.com", 
-                "floor": "42", 
-                "first_name": "Clifford", 
-                "street": "Ethan Land", 
-                "building": "8028", 
-                "phone_number": "+86(8)9135210487", 
-                "postal_code": "01898", 
+                "apartment": "803",
+                "email": "claudette09@exa.com",
+                "floor": "42",
+                "first_name": "Clifford",
+                "street": "Ethan Land",
+                "building": "8028",
+                "phone_number": "+86(8)9135210487",
+                "postal_code": "01898",
                 "extra_description": "8 Ram , 128 Giga",
-                "city": "Jaskolskiburgh", 
-                "country": "CR", 
-                "last_name": "Nicolas", 
+                "city": "Jaskolskiburgh",
+                "country": "CR",
+                "last_name": "Nicolas",
                 "state": "Utah"
             },
-                "shipping_details": {
-                    "notes" : " test",
-                    "number_of_packages": 1,
-                    "weight" : 1,
-                    "weight_unit" : "Kilogram",
-                    "length" : 1,
-                    "width" :1,
-                    "height" :1,
-                    "contents" : "product of some sorts"
-                }
+            "shipping_details": {
+                "notes": " test",
+                "number_of_packages": 1,
+                "weight": 1,
+                "weight_unit": "Kilogram",
+                "length": 1,
+                "width": 1,
+                "height": 1,
+                "contents": "product of some sorts"
             }
+        }
 
-        r = requests.post(url, json = context)
+        r = requests.post(url, json=context)
         id = r.json().get('id')
         msg = r.json().get('message')
         if not id:
@@ -134,46 +136,46 @@ class Payment(models.Model):
         print('order_id', order_id)
 
         billing_data = {
-                        "apartment": "803", 
-                        "email": "claudette09@exa.com", 
-                        "floor": "42", 
-                        "first_name": "Clifford", 
-                        "street": "Ethan Land", 
-                        "building": "8028", 
-                        "phone_number": "+86(8)9135210487", 
-                        "shipping_method": "PKG", 
-                        "postal_code": "01898", 
-                        "city": "Jaskolskiburgh", 
-                        "country": "CR", 
-                        "last_name": "Nicolas", 
+            "apartment": "803",
+            "email": "claudette09@exa.com",
+            "floor": "42",
+            "first_name": "Clifford",
+            "street": "Ethan Land",
+            "building": "8028",
+                        "phone_number": "+86(8)9135210487",
+                        "shipping_method": "PKG",
+                        "postal_code": "01898",
+                        "city": "Jaskolskiburgh",
+                        "country": "CR",
+                        "last_name": "Nicolas",
                         "state": "Utah"
-                    }
+        }
         context = {
-                    "auth_token": token,
-                    "amount_cents": "100", 
-                    "expiration": 3600, 
-                    "order_id": order_id,
-                    "billing_data": billing_data, 
-                    "currency": "EGP", 
-                    "integration_id": integration_id,
-                    "lock_order_when_paid": "false"
-                    }
+            "auth_token": token,
+            "amount_cents": "100",
+            "expiration": 3600,
+            "order_id": order_id,
+            "billing_data": billing_data,
+            "currency": "EGP",
+            "integration_id": integration_id,
+            "lock_order_when_paid": "false"
+        }
 
-        r = requests.post(url, json = context)
+        r = requests.post(url, json=context)
         token = r.json().get('token')
         if not token:
             return False, token
         return True,  token
 
     def get_paymob_token(self, merchant_order_id):
-        """ get paymob token through paymob 3 api calls"""
         integration_id = '3065725'
         result = self._paymob_first_api_call()
         if not result[0]:
             return result
         token = result[1]
 
-        result = self._paymob_seccond_api_call(token=token, merchant_order_id=merchant_order_id)
+        result = self._paymob_seccond_api_call(
+            token=token, merchant_order_id=merchant_order_id)
         if not result[0]:
             return result
         order_id = result[1]
--- order/views.py
+++ order/views.py
@@ -1,44 +1,36 @@
-from django.shortcuts import render, HttpResponseRedirect, HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-import hmac
 import hashlib
+import hmac
+
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.views.decorators.csrf import csrf_exempt
 
 from . import models
 
 
 def payment_view(request):
-    """ payment wity paymob 
-    
-        - send 3 apis to paymob to get token.
-        - if succeeded to get token:
-            - redirect to paymob iframe to pay.
-            - validate payment with paymob and display result to client
-        - if failed
-            - display failure alert.
-    """
     new_order = models.Order(name='newOrder', user=request.user)
     new_order.save()
     merchant_order_id = new_order.id
     payment = models.Payment()
-    status, payment_key = payment.get_paymob_token(merchant_order_id=merchant_order_id)
+    status, payment_key = payment.get_paymob_token(
+        merchant_order_id=merchant_order_id)
 
-    
     if status:
         iframe_page = f'https://accept.paymob.com/api/acceptance/iframes/698300?payment_token={payment_key}'
         return HttpResponseRedirect(iframe_page)
-    
+
     result = 'Payment Failed .. '
     if payment_key == 'duplicate':
         result += "Duplicate Product ID"
     return render(
-            request=request,
-            template_name='order/result.html',
-            context={'result':result, 'status':status}
-        )
+        request=request,
+        template_name='order/result.html',
+        context={'result': result, 'status': status}
+    )
+
 
 @csrf_exempt
 def post_pay(request):
-    """ paymob hmac validation"""
 
     hmac_secret = 'FDDE8D8FB185CDE6AA6CCF5D5BD8FBD8'
     hmac_fields = [
@@ -65,15 +57,15 @@ def post_pay(request):
     ]
     hmac_fields.sort()
     result = 'post_request'
-    if request.method == 'GET': 
+    if request.method == 'GET':
         sent_hmac = request.GET.get('hmac')
         concatenated_str = ''.join(request.GET.get(i) for i in hmac_fields)
-        generated_hmac = hmac.new(hmac_secret.encode('utf-8'), concatenated_str.encode('utf8'), hashlib.sha512).hexdigest()
+        generated_hmac = hmac.new(hmac_secret.encode(
+            'utf-8'), concatenated_str.encode('utf8'), hashlib.sha512).hexdigest()
         result = hmac.compare_digest(generated_hmac, sent_hmac)
         return render(
             request=request,
             template_name='order/result.html',
-            context={'result':result, 'status': True}
+            context={'result': result, 'status': True}
         )
     return HttpResponse(f'<h6>RESULT: {result}</h6>')
-    
